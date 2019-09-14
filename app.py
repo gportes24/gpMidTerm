810921f54427d1aa6513163e4dd2d534199a84f6
German Portes <germanportes@Germans-MacBook-Air.local>
2019-09-14 14:32:46 -0400

    all again

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ app.layout = html.Div(children=[
             html.Div(textbody, style={
                 'padding': '12px',
                 'font-size': '15px',
-                'width': '90px',
+                'width': '180px',
                 'height': '90px',
                 'border': 'thin blue solid',
                 'color': 'rgb(159, 78, 78)',
